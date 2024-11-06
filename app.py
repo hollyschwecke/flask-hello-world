a5c62ecdf4d0d0c5b8a7483cf3dba032b3beb54e
Holly Schwecke <hollyschwecke@gmail.com>
2024-11-06 22:44:30 +0000

    fixed db_test

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def hello_world():
     return 'Hello World from Holly Schwecke in 3308!'
 
 @app.route('/db_test')
-def testing:
+def testing():
     conn = psycopg2.connect("postgresql://schwecke_lab10_database_user:4NeoO85Ipw8AavH2X3IOOflP6aOlVbfA@dpg-csluug1u0jms73b9eflg-a/schwecke_lab10_database")
     conn.close()
     return "Database Connection Successful"
